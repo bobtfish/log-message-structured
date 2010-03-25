7cfffb3b916d8696e31fbe9d71cdb182b03083e7
Tomas Doran <bobtfish@bobtfish.net>
2010-03-25 22:06:37 +0000

    Skeleton

---
+++ lib/Log/Message/Structured.pm
@@ -0,0 +1,72 @@
+package Log::Message::Structured;
+use MooseX::Role::WithOverloading;
+use MooseX::Storage;
+use DateTime;
+use MooseX::Types::ISO8601 qw/ ISO8601DateTimeStr /;
+use Sys::Hostname ();
+use namespace::clean -except => 'meta';
+
+use overload
+    q{""}    => 'as_string',
+    fallback => 1;
+
+with Storage('format' => 'JSON');
+
+has date => (
+    is => 'ro',
+    isa => ISO8601DateTimeStr,
+    default => sub { DateTime->new },
+    coerce => 1,
+);
+
+has hostname => (
+    is => 'ro',
+    default => sub { Sys::Hostname::hostname() },
+);
+
+requires 'as_string';
+
+1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+Log::Message::Structured - Simple structured log messages
+
+=head1 SYNOPSIS
+
+    package My::Log::Event;
+    use Moose;
+    use namespace::autoclean;
+
+    with 'Log::Message::Structured';
+
+    has foo => ( is => 'ro', required => 1 );
+
+    sub as_string { shift->freeze }
+
+    ... elsewhere ...
+
+    use aliased 'My::Log::Event';
+
+    $logger->log(message => Event->new( foo => "bar" ));
+    # Logs:
+    XXXX FIXME XXXXX
+
+=head1 DESCRIPTION
+
+FIXME
+
+=head1 AUTHOR AND COPYRIGHT
+
+Tomas Doran C<< <bobtfish@bobtfish.net> >>.
+
+=head1 LICENSE
+
+Licensed under the same terms as perl itself.
+
+=cut
+
