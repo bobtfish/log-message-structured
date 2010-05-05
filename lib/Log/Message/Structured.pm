6446d0502178d052456238342caedb6259dbf72c
Tomas Doran (t0m) <t0m@state51.co.uk>
2010-05-05 20:07:19 +0100

    Bump versions

--- lib/Log/Message/Structured.pm
+++ lib/Log/Message/Structured.pm
@@ -6,7 +6,7 @@ use MooseX::Types::ISO8601 qw/ ISO8601DateTimeStr /;
 use Sys::Hostname ();
 use namespace::clean -except => 'meta';
 
-our $VERSION = '0.001';
+our $VERSION = '0.002';
 $VERSION = eval $VERSION;
 
 use overload
