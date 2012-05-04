5df2ac9119bdf34803044a9e9ce8bfad2b085d23
Damien Krotkine <dkrotkine@weborama.com>
2012-05-04 15:07:12 +0200

    version 0.008

--- lib/Log/Message/Structured.pm
+++ lib/Log/Message/Structured.pm
@@ -2,7 +2,7 @@ package Log::Message::Structured;
 use MooseX::Role::WithOverloading;
 use namespace::clean -except => 'meta';
 
-our $VERSION = '0.007';
+our $VERSION = '0.008';
 $VERSION = eval $VERSION;
 
 use overload
